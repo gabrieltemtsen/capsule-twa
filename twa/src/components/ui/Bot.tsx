494363f5974b1d11805edf0558a9bac02e4c4d68
gabrieltemtsen <gabrieltemtsen@gmail.com>
2025-01-23 14:43:01 +0100

    update

--- twa/src/components/ui/Bot.tsx
+++ twa/src/components/ui/Bot.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card, CardContent } from "./card";
 import { Button } from "./button";
-import { SEND_SESSION_TO_SERVER } from "../../lib/utils";
+import { SEND_SESSION_TO_SERVER, TEST_SERVER_HELLO } from "../../lib/utils";
 
 interface BotCardProps {
   username: string;
@@ -31,6 +31,12 @@ export const BotCard: React.FC<BotCardProps> = ({
 
             <li>Ask Bot to make transactions</li>
             <li>Ask Bot for Portfolio Balance</li>
+
+            <Button
+            onClick={()=> {TEST_SERVER_HELLO();} }
+            >
+                TEST SERVER
+            </Button>
            
           </div>
         </div>
